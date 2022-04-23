ac5755b0ac9fa9b1a2077f464c2d6c426efc8f75
jan <jcjvanroestel@gmail.com>
2022-04-23 15:39:37 +0200

    first versions of dl code

---
+++ negative_alert_query.py
@@ -0,0 +1,166 @@
+'''
+Query Kowalski for alerts that could be cataclysmic variables
+'''
+
+import os
+import json
+import time
+import datetime
+import pandas as pd
+import numpy as np
+from bson.json_util import dumps, loads 
+from penquins import Kowalski
+from astropy.time import Time
+
+
+
+
+def make_queries(field):
+    """ make the queries"""
+    
+    # define the projection; the fields to get from the database
+    projection = {"objectId": 1,
+                  "candidate.rcid": 1,
+                  "candidate.field": 1,
+                  "candidate.ra": 1,
+                  "candidate.dec": 1,
+                  "candidate.jd": 1,
+                  "candidate.ndethist": 1,
+                  "candidate.jdstarthist": 1,
+                  "candidate.jdendhist": 1,
+                  "candidate.magpsf": 1,
+                  "candidate.sigmapsf": 1,
+                  "candidate.fid": 1,
+                  "candidate.programid": 1,
+                  "candidate.isdiffpos": 1,
+                  "candidate.ssdistnr": 1,
+                  "candidate.rb": 1,
+                  "candidate.drb": 1,
+                  "candidate.classtar": 1,
+                  "candidate.distnr": 1,   
+                  "candidate.nbad": 1,   
+                  "candidate.magnr": 1,   
+                  "candidate.distpsnr1": 1,   
+                  "candidate.sgscore1": 1,
+                  "candidate.sgmag1": 1,
+                  "candidate.srmag1": 1,
+                  "candidate.simag1": 1,
+                  "candidate.szmag1": 1,
+                  }
+
+    # define the filters
+
+    basefilter = {'candidate.jd': {'$gt': 2450000.5, '$lt': 2500000.0},
+                  'candidate.field': int(field), 
+                  '$or': [{'classifications.braai': {'$gt': 0.5}}, {'candidate.drb': {'$gt': 0.5} }],
+                 }
+
+    alertfilter1 = {    'candidate.distnr': {'$lt': 1.5},
+                        'candidate.magnr': {'$gt': 14.0},
+                        'candidate.isdiffpos': {'$in': ['f','0']}
+                       }
+
+
+
+    # add the basefilter to 
+    alertfilter1.update(basefilter)
+
+    # actually make the queries
+    qs = list([])
+    for alertfilter in [alertfilter1,]:
+        q = {"query_type": "find",
+             "query": {
+                 "catalog": "ZTF_alerts",
+                 "filter": alertfilter,
+                 "projection": projection,
+                 },
+                 "kwargs": {
+                     "hint": "jd_field_rb_drb_braai_ndethhist_magpsf_isdiffpos",
+                     "max_time_ms": 10000000
+                 }
+                 }
+
+        # append the filter to list of filters
+        qs.append(q)
+
+    return qs
+
+
+
+def get_alertdata(q,k):
+    """ run an alertquery and put the result in a df
+    """
+    
+    # get data
+    print('starting query')
+    r = k.query(query=q)
+    #print(r)
+    data = r.get('data')
+    print(len(data))
+
+    # combine into a single dataframe
+    output = pd.concat([
+        pd.DataFrame([d['objectId'] for d in data],columns=['objectId',]),
+        pd.DataFrame([d['candidate'] for d in data]) ],axis=1)
+       
+    return output
+
+
+
+def run_CVqueries(k,field):
+    """ run all queries, combine the results, and do some cleaning
+    """
+
+    # make the queries
+    qs = make_queries(field)
+
+    # run queries
+    dfs = [get_alertdata(q,k) for q in qs]
+    output = pd.concat(dfs,ignore_index=True)
+
+    # clean
+    output.replace(-999.0,np.nan,inplace=True)
+
+    # correct magnitude
+    if 'magpsf' in output.keys():
+        output['mag'] = -2.5*np.log10(10**(-0.4*output['magpsf']) + 10**(-0.4*output['magnr']))
+
+    return output
+
+
+if __name__ == "__main__":
+    import argparse
+    import os
+
+    parser = argparse.ArgumentParser(description='Download candidates by field')
+    parser.add_argument('-o', action='store_true')
+    args = parser.parse_args()
+
+    # savedir
+    savedir = "./queries"
+
+    # make a directory to store the query results
+    os.system("mkdir -p %s" %savedir)
+
+    # login details
+    with open('secrets.json', 'r') as f:
+        secrets = json.load(f)
+
+    # setup the connection to the kowalski database
+    k = Kowalski(**secrets['kowalski'], verbose=False)
+
+    # 
+    fields = np.r_[np.arange(244,880)[::-1],np.arange(1240,1895)[::-1]]
+
+    for f in fields:
+        outname = "%s/query_fid%0.4d.csv" %(savedir,f)
+        # skip if file is there and no overwrite
+        if not args.o and os.path.exists(outname):
+            print('Skipping field %d' %f)
+            continue
+        print('Query field %d' %f)
+
+        output = run_CVqueries(k,f)
+        output.to_csv(outname,index=False)
+
+
